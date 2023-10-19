367d9a066fceb9d2e3341c654f0b47736fe86338
lidiya207 <137820814+lidiya207@users.noreply.github.com>
2023-10-19 07:45:47 -0700

    Time: 39 ms (82.21%), Space: 13.1 MB (98.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -13,7 +13,7 @@ class Solution(object):
         Head = ListNode(0)
         curr = Head
         carry = 0
-        while l1 or l2 or carry: # O(n)
+        while l1 or l2 or carry: 
             val1 = (l1.val if l1 else 0)
             val2 = (l2.val if l2 else 0)
             carry, out = divmod(val1+val2 + carry, 10)
