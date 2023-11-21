ffcf8aa8f06be399cd41540f14cc9ec764aa8e32
lidiya207 <137820814+lidiya207@users.noreply.github.com>
2023-11-21 09:21:11 +0300

    Time: 48 ms (28.05%), Space: 16.5 MB (23.13%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -4,18 +4,12 @@ class Solution:
         open_count = 0
         close_count = 0
         start = 0  
-
         for i in range(len(s)):
             if s[i] == "(":
                 open_count += 1
             elif s[i] == ")":
                 close_count += 1
-
-          
-            if open_count == close_count:
-               
-                res += s[start + 1:i]
-               
+            if open_count == close_count: 
+                res += s[start + 1:i] 
                 start = i + 1
-
         return res
